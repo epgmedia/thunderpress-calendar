1a76072856e75afbdb3afa4c433e6a259f5c5c10
Annapurna <digital@epgmediallc.com>
2014-04-29 10:32:19 -0400

    More security plugins.

--- wp-content/themes/thunder-press-calendar/functions.php
+++ wp-content/themes/thunder-press-calendar/functions.php
@@ -20,3 +20,11 @@ add_action('draft_to_publish', 'autoset_featured');
 add_action('new_to_publish', 'autoset_featured');
 add_action('pending_to_publish', 'autoset_featured');
 add_action('future_to_publish', 'autoset_featured');
+
+
+add_action('admin_init', 'rw_remove_dashboard_widgets');
+function rw_remove_dashboard_widgets() {
+ remove_meta_box('dashboard_plugins', 'dashboard', 'normal');   // plugins 
+ remove_meta_box('dashboard_quick_press', 'dashboard', 'normal');  // quick press
+ remove_meta_box('dashboard_recent_drafts', 'dashboard', 'normal');  // recent drafts
+}
